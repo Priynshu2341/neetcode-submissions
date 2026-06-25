92ad377247fcc5c0865681161dfd3f3e5f34a8d4
Priyanshu Kushwaha <Priyanshukushwaha26@gmail.com>
2026-06-25 17:19:44 +0530

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+       int maxProfit = 0;
+       int buydate = prices[0];
+
+       for(int i = 1; i < prices.length; i++){
+         if(prices[i] < buydate ){
+            buydate = prices[i];
+         }
+         int profit = prices[i] - buydate;
+
+         maxProfit = Math.max(profit,maxProfit);
+
+       } 
+
+       return maxProfit;
+
+    }
+}
